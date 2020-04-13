e804e0c709e3d260c5f68a538801c24a5cbda29a
MO NAN <651932351@qq.com>
2020-04-13 21:51:50 +0800

    add account & conf generate (#24)
    * add generator
    
    * remote hardcode
    
    Co-authored-by: ancelmo <ancelmo@ANCELMOOA2.webank.com>

--- src/main/java/com/webank/wecross/stub/bcos/BCOSStubFactory.java
+++ src/main/java/com/webank/wecross/stub/bcos/BCOSStubFactory.java
@@ -6,12 +6,21 @@ import com.webank.wecross.stub.Driver;
 import com.webank.wecross.stub.Stub;
 import com.webank.wecross.stub.StubFactory;
 import com.webank.wecross.stub.bcos.account.BCOSAccountFactory;
+import java.io.File;
+import java.io.FileWriter;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.Security;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.util.io.pem.PemObject;
+import org.bouncycastle.util.io.pem.PemWriter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Stub("BCOS2.0")
 public class BCOSStubFactory implements StubFactory {
-    private static final Logger logger = LoggerFactory.getLogger(BCOSStubFactory.class);
+    private Logger logger = LoggerFactory.getLogger(BCOSStubFactory.class);
 
     @Override
     public Driver newDriver() {
@@ -40,8 +49,97 @@ public class BCOSStubFactory implements StubFactory {
     }
 
     @Override
-    public void generateAccount(String s, String[] strings) {}
+    public void generateAccount(String path, String[] args) {
+        try {
+            // Write pem file
+            Security.addProvider(new BouncyCastleProvider());
+            KeyPairGenerator keyPairGenerator =
+                    KeyPairGenerator.getInstance("EC", Security.getProvider("BC"));
+
+            keyPairGenerator.initialize(256);
+            KeyPair keyPair = keyPairGenerator.generateKeyPair();
+            PrivateKey ecPrivateKey = keyPair.getPrivate();
+
+            String keyFile = path + "/account.key";
+            File file = new File(keyFile);
+
+            if (!file.createNewFile()) {
+                logger.error("Key file exists! {}", keyFile);
+                return;
+            }
+
+            PemWriter pemWriter = new PemWriter(new FileWriter(file));
+            try {
+                pemWriter.writeObject(new PemObject("PRIVATE KEY", ecPrivateKey.getEncoded()));
+            } finally {
+                pemWriter.close();
+            }
+
+            String accountTemplate =
+                    "[account]\n"
+                            + "    type='BCOS2.0'\n"
+                            + "    accountFile='account.key'\n"
+                            + "    password=''";
+            String confFilePath = path + "/account.toml";
+            File confFile = new File(confFilePath);
+            if (!confFile.createNewFile()) {
+                logger.error("Conf file exists! {}", confFile);
+                return;
+            }
+
+            FileWriter fileWriter = new FileWriter(confFile);
+            try {
+                fileWriter.write(accountTemplate);
+            } finally {
+                fileWriter.close();
+            }
+
+        } catch (Exception e) {
+            logger.error("Exception: ", e);
+        }
+    }
 
     @Override
-    public void generateConnection(String s, String[] strings) {}
+    public void generateConnection(String path, String[] args) {
+        try {
+            String accountTemplate =
+                    "[common]\n"
+                            + ""
+                            + "    type = 'BCOS2.0' # BCOS\n"
+                            + "\n"
+                            + "[chain]\n"
+                            + "    groupId = 1 # default 1\n"
+                            + "    chainId = 1 # default 1\n"
+                            + "    enableGM = false # default false\n"
+                            + "\n"
+                            + "[channelService]\n"
+                            + "    caCert = 'ca.crt'\n"
+                            + "    sslCert = 'sdk.crt'\n"
+                            + "    sslKey = 'sdk.key'\n"
+                            + "    timeout = 300000  # ms, default 60000ms\n"
+                            + "    connectionsStr = ['127.0.0.1:20200']\n"
+                            + "\n"
+                            + "# resources is a list\n"
+                            + "[[resources]]\n"
+                            + "    # name cannot be repeated\n"
+                            + "    name = 'HelloWeCross'\n"
+                            + "    type = 'BCOS_CONTRACT'\n"
+                            + "    contractAddress = '0x0'";
+            String confFilePath = path + "/stub.toml";
+            File confFile = new File(confFilePath);
+            if (!confFile.createNewFile()) {
+                logger.error("Conf file exists! {}", confFile);
+                return;
+            }
+
+            FileWriter fileWriter = new FileWriter(confFile);
+            try {
+                fileWriter.write(accountTemplate);
+            } finally {
+                fileWriter.close();
+            }
+        } catch (Exception e) {
+            logger.error("Exception: ", e);
+        }
+    }
 }
