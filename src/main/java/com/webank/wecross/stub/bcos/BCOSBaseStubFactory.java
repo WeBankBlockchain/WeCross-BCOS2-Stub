877826648361d73c55652b39c5e22e99b9f8b94e
kyonRay <rui.kwok@outlook.com>
2020-10-19 19:35:17 +0800

    <perf>(StubFactory): add [sealer] config in generateConnection.

--- src/main/java/com/webank/wecross/stub/bcos/BCOSBaseStubFactory.java
+++ src/main/java/com/webank/wecross/stub/bcos/BCOSBaseStubFactory.java
@@ -241,7 +241,8 @@ public class BCOSBaseStubFactory implements StubFactory {
                             + "    sslKey = 'sdk.key'\n"
                             + "    timeout = 300000  # ms, default 60000ms\n"
                             + "    connectionsStr = ['127.0.0.1:20200']\n"
-                            + "\n";
+                            + "\n"
+                            + "[sealer]\n";
             String confFilePath = path + "/stub.toml";
             File confFile = new File(confFilePath);
             if (!confFile.createNewFile()) {
