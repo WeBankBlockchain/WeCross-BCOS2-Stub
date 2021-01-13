185b628fe43ad4c0fe11d59f9526f34b0bbd84a0
octopus <912554887@qq.com>
2021-01-14 00:16:44 +0800

    supports all bcos versions

--- src/main/java/com/webank/wecross/stub/bcos/web3j/Web3jUtility.java
+++ src/main/java/com/webank/wecross/stub/bcos/web3j/Web3jUtility.java
@@ -6,11 +6,9 @@ import java.util.List;
 import org.fisco.bcos.channel.client.Service;
 import org.fisco.bcos.channel.handler.ChannelConnections;
 import org.fisco.bcos.channel.handler.GroupChannelConnectionsConfig;
-import org.fisco.bcos.fisco.EnumNodeVersion;
 import org.fisco.bcos.web3j.crypto.EncryptType;
 import org.fisco.bcos.web3j.protocol.Web3j;
 import org.fisco.bcos.web3j.protocol.channel.ChannelEthereumService;
-import org.fisco.bcos.web3j.protocol.core.methods.response.NodeVersion;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
@@ -19,8 +17,6 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 public class Web3jUtility {
 
     private static final Logger logger = LoggerFactory.getLogger(Web3jUtility.class);
-
-    private Web3jUtility() {}
 
     public static ThreadPoolTaskExecutor build(int threadNum, int queueCapacity, String name) {
         logger.info(
@@ -36,86 +32,6 @@ public class Web3jUtility {
         threadPool.setThreadNamePrefix(name);
         threadPool.initialize();
         return threadPool;
-    }
-
-    /**
-     * Initialize the web3j service with check the check the configuration
-     *
-     * @param bcosStubConfig
-     * @return
-     * @throws Exception
-     */
-    public static Web3j initWeb3j(BCOSStubConfig bcosStubConfig) throws Exception {
-
-        logger.info("BCOSStubConfig: {}", bcosStubConfig);
-
-        BCOSStubConfig.ChannelService channelService = bcosStubConfig.getChannelService();
-        Web3j web3j = initWeb3j(channelService);
-
-        NodeVersion.Version version = web3j.getNodeVersion().send().getNodeVersion();
-
-        logger.info("NodeVersion: {}", version);
-
-        // check version
-        checkVersion(version);
-        // check config
-        checkConfig(version, bcosStubConfig.getType());
-
-        return web3j;
-    }
-
-    /**
-     * Check the stub config type
-     *
-     * @param version
-     * @param stubType
-     */
-    public static void checkConfig(NodeVersion.Version version, String stubType) throws Exception {
-        boolean isGMStub = stubType.toLowerCase().contains("gm");
-        boolean isGMNode = version.getVersion().toLowerCase().contains("gm");
-        if (logger.isDebugEnabled()) {
-            logger.debug(" isGMStub: {}, isGMNode: {}", isGMStub, isGMNode);
-        }
-        if (!(isGMStub == isGMNode)) {
-            throw new Exception(
-                    "Please check config "
-                            + "stub.toml common::type field, change to \""
-                            + (isGMNode ? "GM_BCOS2.0" : "BCOS2.0")
-                            + "\"");
-        }
-    }
-
-    /**
-     * Check the node version information, 2.4.0+ supported
-     *
-     * @param version
-     * @throws Exception
-     */
-    public static void checkVersion(NodeVersion.Version version) throws Exception {
-
-        String supportedVersionStr = version.getSupportedVersion();
-        String nodeVersionStr = version.getVersion();
-
-        EnumNodeVersion.Version supportedVersion =
-                EnumNodeVersion.getClassVersion(supportedVersionStr);
-
-        /*2.4.0 gm or 2.4.0*/
-        String[] strings = nodeVersionStr.split(" ");
-        EnumNodeVersion.Version nodeVersion = EnumNodeVersion.getClassVersion(strings[0]);
-
-        // must not below than 2.4.0
-        if (!(supportedVersion.getMajor() == 2 && supportedVersion.getMinor() >= 4)) {
-            throw new Exception(
-                    "FISCO BCOS supported version is not supported, version must not below than 2.4.0, but current is "
-                            + supportedVersionStr);
-        }
-
-        // must not below than 2.4.0
-        if (!(nodeVersion.getMajor() == 2 && nodeVersion.getMinor() >= 4)) {
-            throw new Exception(
-                    "FISCO BCOS version is not supported, version must not below than 2.4.0, but current is "
-                            + nodeVersionStr);
-        }
     }
 
     /**
