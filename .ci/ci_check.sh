f2b425b88c5eabfbde9e8732feebdad2337515d6
Kyon <32325790+kyonRay@users.noreply.github.com>
2023-03-14 20:10:37 +0800

    <fix>(build): update version to 1.3.0. (#202)

--- .ci/ci_check.sh
+++ .ci/ci_check.sh
@@ -10,7 +10,7 @@ echo " Not SM NODE ============>>>> "
 echo " Not SM NODE ============>>>> "
 
 # download build_chain.sh to build fisco-bcos block chain
-curl -LO https://github.com/FISCO-BCOS/FISCO-BCOS/releases/download/v2.8.0/build_chain.sh && chmod u+x build_chain.sh
+curl -LO https://github.com/FISCO-BCOS/FISCO-BCOS/releases/download/v2.9.1/build_chain.sh && chmod u+x build_chain.sh
 echo "127.0.0.1:4 agency1 1,2,3" > ipconf
 bash build_chain.sh -f ipconf
 ./nodes/127.0.0.1/fisco-bcos -v
@@ -42,7 +42,7 @@ echo " SM NODE ============>>>> "
 echo " SM NODE ============>>>> "
 
 # download build_chain.sh to build fisco-bcos block chain
-curl -LO https://github.com/FISCO-BCOS/FISCO-BCOS/releases/download/v2.6.0/build_chain.sh && chmod u+x build_chain.sh
+curl -LO https://github.com/FISCO-BCOS/FISCO-BCOS/releases/download/v2.9.1/build_chain.sh && chmod u+x build_chain.sh
 echo "127.0.0.1:4 agency1 1,2,3" > ipconf
 bash build_chain.sh -f ipconf -g
 ./nodes/127.0.0.1/fisco-bcos -v
@@ -76,7 +76,7 @@ echo " SM NODE, SM SSL============>>>> "
 echo " SM NODE, SM SSL============>>>> "
 
 # download build_chain.sh to build fisco-bcos block chain
-curl -LO https://github.com/FISCO-BCOS/FISCO-BCOS/releases/download/v2.6.0/build_chain.sh && chmod u+x build_chain.sh
+curl -LO https://github.com/FISCO-BCOS/FISCO-BCOS/releases/download/v2.9.1/build_chain.sh && chmod u+x build_chain.sh
 echo "127.0.0.1:4 agency1 1,2,3" > ipconf
 bash build_chain.sh -f ipconf -g -G
 ./nodes/127.0.0.1/fisco-bcos -v
