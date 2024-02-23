47ef05e224791f712e4f1fc3c55172cc7e809459
Kyon <32325790+kyonRay@users.noreply.github.com>
2024-02-23 09:56:50 +0800

    <fix>(project): upgrade project spring, bouncycastle deps version. (#211)

--- .ci/ci_check.sh
+++ .ci/ci_check.sh
@@ -3,14 +3,22 @@ set -e
 
 # gradle build check
 bash gradlew build
-
+get_sed_cmd()
+{
+  local sed_cmd="sed -i"
+  if [ "$(uname)" == "Darwin" ];then
+        sed_cmd="sed -i .bkp"
+  fi
+  echo "$sed_cmd"
+}
+sed_cmd=$(get_sed_cmd)
 # Non SM node test
 echo " Not SM NODE ============>>>> "
 echo " Not SM NODE ============>>>> "
 echo " Not SM NODE ============>>>> "
 
 # download build_chain.sh to build fisco-bcos block chain
-curl -LO https://github.com/FISCO-BCOS/FISCO-BCOS/releases/download/v2.9.1/build_chain.sh && chmod u+x build_chain.sh
+curl -LO https://github.com/FISCO-BCOS/FISCO-BCOS/releases/download/v2.10.1/build_chain.sh && chmod u+x build_chain.sh
 echo "127.0.0.1:4 agency1 1,2,3" > ipconf
 bash build_chain.sh -f ipconf
 ./nodes/127.0.0.1/fisco-bcos -v
@@ -42,7 +50,7 @@ echo " SM NODE ============>>>> "
 echo " SM NODE ============>>>> "
 
 # download build_chain.sh to build fisco-bcos block chain
-curl -LO https://github.com/FISCO-BCOS/FISCO-BCOS/releases/download/v2.9.1/build_chain.sh && chmod u+x build_chain.sh
+curl -LO https://github.com/FISCO-BCOS/FISCO-BCOS/releases/download/v2.10.1/build_chain.sh && chmod u+x build_chain.sh
 echo "127.0.0.1:4 agency1 1,2,3" > ipconf
 bash build_chain.sh -f ipconf -g
 ./nodes/127.0.0.1/fisco-bcos -v
@@ -53,7 +61,7 @@ bash nodes/127.0.0.1/start_all.sh
 mkdir -p src/integTest/resources/chains/bcos
 cp -r nodes/127.0.0.1/sdk/* src/integTest/resources/chains/bcos
 cp src/test/resources/stub.toml src/integTest/resources/chains/bcos/
-sed -i.bak 's/BCOS2/GM_BCOS2/g' src/integTest/resources/chains/bcos/stub.toml
+${sed_cmd} 's/BCOS2/GM_BCOS2/g' src/integTest/resources/chains/bcos/stub.toml
 
 cat src/integTest/resources/chains/bcos/stub.toml
 mkdir -p src/integTest/resources/accounts
@@ -76,7 +84,7 @@ echo " SM NODE, SM SSL============>>>> "
 echo " SM NODE, SM SSL============>>>> "
 
 # download build_chain.sh to build fisco-bcos block chain
-curl -LO https://github.com/FISCO-BCOS/FISCO-BCOS/releases/download/v2.9.1/build_chain.sh && chmod u+x build_chain.sh
+curl -LO https://github.com/FISCO-BCOS/FISCO-BCOS/releases/download/v2.10.1/build_chain.sh && chmod u+x build_chain.sh
 echo "127.0.0.1:4 agency1 1,2,3" > ipconf
 bash build_chain.sh -f ipconf -g -G
 ./nodes/127.0.0.1/fisco-bcos -v
@@ -87,7 +95,8 @@ bash nodes/127.0.0.1/start_all.sh
 mkdir -p src/integTest/resources/chains/bcos
 cp -r nodes/127.0.0.1/sdk/* src/integTest/resources/chains/bcos
 cp src/test/resources/stub.toml src/integTest/resources/chains/bcos/
-sed -i.bak -e 's/BCOS2/GM_BCOS2/g' -e 's/gmConnectEnable = false/gmConnectEnable = true/g' src/integTest/resources/chains/bcos/stub.toml
+${sed_cmd} -e 's/BCOS2/GM_BCOS2/g' src/integTest/resources/chains/bcos/stub.toml
+${sed_cmd} -e 's/gmConnectEnable = false/gmConnectEnable = true/g' src/integTest/resources/chains/bcos/stub.toml
 
 cat src/integTest/resources/chains/bcos/stub.toml
 mkdir -p src/integTest/resources/accounts
