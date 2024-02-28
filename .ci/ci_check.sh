c2c6a712cf12a2ef30cdac969d1f71d8d743fb42
Kyon <32325790+kyonRay@users.noreply.github.com>
2024-02-28 14:12:38 +0800

    <fix>(build): update build for secure issues. (#212)

--- .ci/ci_check.sh
+++ .ci/ci_check.sh
@@ -18,7 +18,7 @@ echo " Not SM NODE ============>>>> "
 echo " Not SM NODE ============>>>> "
 
 # download build_chain.sh to build fisco-bcos block chain
-curl -LO https://github.com/FISCO-BCOS/FISCO-BCOS/releases/download/v2.10.1/build_chain.sh && chmod u+x build_chain.sh
+curl -LO https://github.com/FISCO-BCOS/FISCO-BCOS/releases/download/v2.9.1/build_chain.sh && chmod u+x build_chain.sh
 echo "127.0.0.1:4 agency1 1,2,3" > ipconf
 bash build_chain.sh -f ipconf
 ./nodes/127.0.0.1/fisco-bcos -v
@@ -35,7 +35,7 @@ mkdir -p src/integTest/resources/solidity
 cp src/main/resources/* src/integTest/resources/solidity
 cp src/test/resources/contract/* src/integTest/resources/solidity
 
-bash gradlew integTest
+bash gradlew integTest --info
 bash gradlew jacocoTestReport
 
 # clean
@@ -50,7 +50,7 @@ echo " SM NODE ============>>>> "
 echo " SM NODE ============>>>> "
 
 # download build_chain.sh to build fisco-bcos block chain
-curl -LO https://github.com/FISCO-BCOS/FISCO-BCOS/releases/download/v2.10.1/build_chain.sh && chmod u+x build_chain.sh
+curl -LO https://github.com/FISCO-BCOS/FISCO-BCOS/releases/download/v2.9.1/build_chain.sh && chmod u+x build_chain.sh
 echo "127.0.0.1:4 agency1 1,2,3" > ipconf
 bash build_chain.sh -f ipconf -g
 ./nodes/127.0.0.1/fisco-bcos -v
@@ -70,7 +70,7 @@ cp -r src/test/resources/accounts/gm_bcos src/integTest/resources/accounts/fisco
 mkdir -p src/integTest/resources/solidity
 cp src/main/resources/* src/integTest/resources/solidity
 cp src/test/resources/contract/* src/integTest/resources/solidity
-bash gradlew integTest
+bash gradlew integTest --info
 
 # clean
 bash nodes/127.0.0.1/stop_all.sh
@@ -84,7 +84,7 @@ echo " SM NODE, SM SSL============>>>> "
 echo " SM NODE, SM SSL============>>>> "
 
 # download build_chain.sh to build fisco-bcos block chain
-curl -LO https://github.com/FISCO-BCOS/FISCO-BCOS/releases/download/v2.10.1/build_chain.sh && chmod u+x build_chain.sh
+curl -LO https://github.com/FISCO-BCOS/FISCO-BCOS/releases/download/v2.9.1/build_chain.sh && chmod u+x build_chain.sh
 echo "127.0.0.1:4 agency1 1,2,3" > ipconf
 bash build_chain.sh -f ipconf -g -G
 ./nodes/127.0.0.1/fisco-bcos -v
@@ -105,7 +105,7 @@ cp -r src/test/resources/accounts/gm_bcos src/integTest/resources/accounts/fisco
 mkdir -p src/integTest/resources/solidity
 cp src/main/resources/* src/integTest/resources/solidity
 cp src/test/resources/contract/* src/integTest/resources/solidity
-bash gradlew integTest
+bash gradlew integTest --info
 
 # clean
 bash nodes/127.0.0.1/stop_all.sh
